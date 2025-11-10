d9e92fdf04794b627c10b52df67d67a00b100886
Sagarkumar6204 <bittukumar9006716@gmail.com>
2025-11-10 23:16:09 +0530

    adding css styling and form control for better ui experience

--- models/listing.js
+++ models/listing.js
@@ -1,20 +1,25 @@
 const mongoose = require('mongoose');
-const schema=mongoose.Schema;
-
-const listingSchema=new schema({
-title:{
-    type : String,
-    required : true,
-},
-description:String,
-images:
-{type:String,
-    default:"https://png.pngtree.com/background/20230526/original/pngtree-sunset-in-ocean-palm-tree-clouds-natural-scene-wallpaper-picture-image_2742444.jpg",
-    set:(v)=> v === "" ? "https://png.pngtree.com/background/20230526/original/pngtree-sunset-in-ocean-palm-tree-clouds-natural-scene-wallpaper-picture-image_2742444.jpg" : v,
-},
-price:Number,
-location:String,
-country:String,
+const listingSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    filename: String,
+    url: {
+      type: String,
+      default: "https://png.pngtree.com/background/20230526/original/pngtree-sunset-in-ocean-palm-tree-clouds-natural-scene-wallpaper-picture-image_2742444.jpg",
+      set: (v) =>
+        v === ""
+          ? "https://png.pngtree.com/background/20230526/original/pngtree-sunset-in-ocean-palm-tree-clouds-natural-scene-wallpaper-picture-image_2742444.jpg"
+          : v,
+    },
+  },
+  price: Number,
+  location: String,
+  country: String,
 });
+
 const Listing= mongoose.model("Listing",listingSchema);
 module.exports=Listing;
